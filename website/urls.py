3f0cd5ce56baa92e5fb00a338c38b89886d03722
heitorffonseca <heitor.pessoal1998@gmail.com.br>
2019-05-06 21:26:06 -0300

    resposta

--- website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from website import views
-
-app_name = 'website'
-
-urlpatterns = [
-    path('contato/', views.contato, name = 'contato'),
-    path('', views.index, name = 'index'),
-]
